05272374b480ce1b8fb4bc70bf91145a1d6f8955
Dhairya Ostwal <50984984+dhairyaostwal@users.noreply.github.com>
2021-01-13 01:17:54 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function createNote(noteItem) {
+  return <Note title={noteItem.title} content={noteItem.content} />;
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note title="Sample title" content="Sample Content" />
+      {/* {notes.map(createNote)} */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
